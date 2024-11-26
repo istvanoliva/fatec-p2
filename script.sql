e4cfffe1dd489944801049dfcf618a6928cdc2e7
Istvan Oliva <olivaistvan@gmail.com>
2024-11-26 09:19:27 -0300

    feat(p2): aprovados com pais phds

--- script.sql
+++ script.sql
@@ -8,3 +8,20 @@ CREATE TABLE students (
     salary INT NOT NULL,
     prep_exam INT NOT NULL
 );
+
+CREATE OR REPLACE PROCEDURE aprovados_com_pais_phd()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    aprovados INTEGER;
+BEGIN
+    SELECT COUNT(*)
+    INTO aprovados
+    FROM students
+    WHERE grade <> 0 
+      AND mother_edu = 6 
+      AND father_edu = 6;
+
+    RAISE NOTICE 'Número de alunos aprovados com pais PhD: %', aprovados;
+END;
+$$;
